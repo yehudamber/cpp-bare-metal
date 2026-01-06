28d13dc635e68d8544522d7f7dd22984937a993b
Yehuda Bernáth <yehudamber@gmail.com>
2026-01-06 17:02:19 +0200

    Remove ConcurrentTask and all related code

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,8 @@
-#include <algorithm>
-#include <exception>
-#include <ranges>
-#include <string_view>
-
-#include "concurrent-task.hpp"
 #include "main.hpp"
 #include "uart.hpp"
 
-using namespace std::literals;
-
-ConcurrentTask printer(std::string_view message, int times)
-{
-    for (auto _ : std::views::iota(0, times))
-    {
-        UART::println(message);
-        co_yield ConcurrentTask::yield;
-    }
-}
-
-ConcurrentTask thrower()
-{
-    co_yield ConcurrentTask::yield;
-    co_yield ConcurrentTask::yield;
-    throw std::exception();
-}
-
 int applicationMain()
 {
-    auto tasks = std::array{ printer("Hello", 6), thrower() };
-    UART::println("Created ", tasks.size(), " tasks");
-    while (std::ranges::any_of(tasks, &ConcurrentTask::operator bool))
-    {
-        for (auto& task : tasks)
-        {
-            if (task)
-            {
-                if (!task.resume())
-                {
-                    task = {};
-                }
-            }
-        }
-    }
+    UART::println("Hello World");
     return 0;
 }
