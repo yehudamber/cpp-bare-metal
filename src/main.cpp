a0142df2874a011fd91c66a6fe8c16e685e39de6
Yehuda Bernáth <yehudamber@gmail.com>
2026-01-06 17:24:53 +0200

    Add app logic demonstrating use of memory allocation and exceptions

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include "main.hpp"
 #include "uart.hpp"
 
+#include <memory>
+
+std::unique_ptr<std::byte[]> tryAllocate(std::size_t size)
+{
+    try
+    {
+        auto ptr = std::unique_ptr<std::byte[]>(new std::byte[size]);
+        UART::println("Allocated ", size, " bytes at ", ptr.get());
+        return ptr;
+    }
+    catch (const std::bad_alloc&)
+    {
+        UART::println("Failed to allocate ", size, " bytes");
+        return nullptr;
+    }
+}
+
 int applicationMain()
 {
-    UART::println("Hello World");
+    UART::println("Hello from bare-metal C++ (", __cplusplus, ")!");
+    auto ptr1 = tryAllocate(1000000);
+    auto ptr2 = tryAllocate(100000000);
+    auto ptr3 = tryAllocate(20000000);
     return 0;
 }
